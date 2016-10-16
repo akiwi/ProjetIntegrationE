3b5cc74015b89796fc6c7148c9091449490e96a5
afktoday <morpheus-lacoste@hotmail.com>
2016-10-16 15:25:42 +0200

    V1.2b
    - suppression de la classe TaskConnexion pour la TaskConnect qui fera le
    boulot d'exécuter n'importe quelle requête en fonction du contentValue
    => suppression aussi du bouton test sur la connexion

--- Application/CareWatch/app/src/main/java/be/ephec/groupe3/carewatch/MainActivity.java
+++ Application/CareWatch/app/src/main/java/be/ephec/groupe3/carewatch/MainActivity.java
@@ -1,7 +1,6 @@
 package be.ephec.groupe3.carewatch;
         import android.app.Activity;
         import android.content.ContentValues;
-        import android.content.Context;
         import android.content.Intent;
         import android.os.Bundle;
         import android.util.Log;
@@ -10,22 +9,23 @@ package be.ephec.groupe3.carewatch;
         import android.widget.TextView;
         import android.widget.Toast;
 
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
         import be.ephec.groupe3.carewatch.Task.TaskConnect;
-        import be.ephec.groupe3.carewatch.Task.TaskConnexion;
-//Test
-public class MainActivity extends Activity implements TaskConnexion.CustomConnexion, View.OnClickListener, TaskConnect.CustomConnexion {
+
+public class MainActivity extends Activity implements View.OnClickListener, TaskConnect.CustomConnexion {
     private TextView userName;
     private TextView userPwd;
     private Button btnConnect;
-    private Button btnTest;
 
     private String login = "";
     private String pass = "";
 
     private final String URL_CONNEXION = "http://192.168.0.16/projetintegration/connexion.php?";
 
-    private Boolean debug = true; //passer en mode debug et avoir des infos en plus
-    private Boolean test = true; //active l'interface de test
+    private Boolean debug = false; //passer en mode debug et avoir des infos en plus
+    private Boolean test = false; //active l'interface de test
 
 
     @Override
@@ -37,14 +37,9 @@ public class MainActivity extends Activity implements TaskConnexion.CustomConnex
         userName = (TextView) findViewById(R.id.ET_name);
         userPwd = (TextView) findViewById(R.id.ET_pass);
         btnConnect = (Button) findViewById(R.id.btn_connect);
-        btnTest = (Button) findViewById(R.id.btn_test);
-
-        if(test)
-            btnTest.setVisibility(View.VISIBLE);
 
         //on défini la méthode onClick plus bas et plus joli ATTENTION à bien ajouter l'implements
         btnConnect.setOnClickListener(this);
-        btnTest.setOnClickListener(this);
 
     }
 
@@ -55,34 +50,17 @@ public class MainActivity extends Activity implements TaskConnexion.CustomConnex
             case R.id.btn_connect: {
                 login = userName.getText().toString();
                 pass = userPwd.getText().toString();
-
-                //on créé une nouvelle Tache Asynchrone pour la connexion
-                TaskConnexion connexion = new TaskConnexion(this);
-
-
+                TaskConnect con = new TaskConnect(this);
+                ContentValues cv = new ContentValues();
+                //toujours mettre url dans le content value car important pour la TaskConnect
+                cv.put("url", URL_CONNEXION);
+                cv.put("nameUser", login);
+                cv.put("pwdUser", pass);
                 if ((!login.isEmpty()) && (!pass.isEmpty())) {
                     //on passe l'url et les identifiant à notre tache
-                    connexion.execute(URL_CONNEXION, login, pass);
+                    con.execute(cv);
                 } else
                     Toast.makeText(this, "Les champs ne peuvent pas être vide", Toast.LENGTH_SHORT).show();
-            }
-            case R.id.btn_test: {
-                if (test) {
-                    login = userName.getText().toString();
-                    pass = userPwd.getText().toString();
-                    TaskConnect con = new TaskConnect(this);
-                    ContentValues cv = new ContentValues();
-                    //toujours mettre url dans le content value car important pour la TaskConnect
-                    cv.put("url", URL_CONNEXION);
-                    cv.put("nameUser", login);
-                    cv.put("pwdUser", pass);
-                    if ((!login.isEmpty()) && (!pass.isEmpty())) {
-                        //on passe l'url et les identifiant à notre tache
-                        con.execute(cv);
-                    } else
-                        Toast.makeText(this, "Les champs ne peuvent pas être vide", Toast.LENGTH_SHORT).show();
-
-                }
             }
         }
 
@@ -95,8 +73,17 @@ public class MainActivity extends Activity implements TaskConnexion.CustomConnex
 
     @Override
     public void showResultConnexion(String s){
-        Log.d("Réponse",s);
-        if(s.equals("-1")){
+        String id = "";
+        try {
+            JSONObject jo = new JSONObject(s);
+            id = jo.optString("id").toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("Réponse",id);
+        if(id.equals("-1")){
+            if(debug)
+                Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
             Toast.makeText(this,"Login/mot de passe incorrect",Toast.LENGTH_SHORT).show();
         }
         else{
